1b6f28a9bdd39d009833e40b1f7abd7aef2ebd16
201dreamers <201dreamers@gmail.com>
2019-04-18 22:44:45 +0300

    snakeGame

---
+++ src/MainWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame{
+
+    public MainWindow(){
+        setTitle("Snake");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,320);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+    }
+}
